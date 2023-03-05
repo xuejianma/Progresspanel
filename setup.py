23556c2191860329e72abbe042d98e1abd5780d6
xuejianma <xuejian.ma@gmail.com>
2023-03-05 01:20:11 -0600

    Specify package to avoid including tests in build.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="progresspanel",
-    version="0.1.0",
+    version="0.1.1",
     description="The Progress Panel is a custom tkinter widget that displays a progress bar and buttons (Start/Resume, Pause, Terminate) to control a user-defined task that runs in a separate thread.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,7 +10,7 @@ setup(
     author_email="Xuejian.Ma@gmail.com",
     url="https://github.com/xuejianma/progresspanel",
     license="MIT",
-    packages=find_packages(),
+    packages=["progresspanel"],
     keywords=["tkinter", "progress panel", "progress bar", "threading"],
     test_suite="tests"
 )
